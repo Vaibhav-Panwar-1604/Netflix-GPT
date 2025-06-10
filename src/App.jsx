d7a9316227628de05ecc27673d274bd717a9b7e3
Vaibhav <vaibhav1604@hotmail.com>
2025-06-10 16:06:28 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+
+function App() {
+
+  return (
+    <>      
+       <h2 className="m-3 p-3 bg-red-100 text-7xl font-bold">NetFlix GPT</h2>
+       <h1 className="m-3 p-3 text-cyan-400 text-3xl bg-red-100">Hello</h1>
+    </>
+  )
+}
+
+export default App
